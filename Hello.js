4c87176246f00651d8fdb41c9376548896952ea0
malakazeba <azebamalak76@gmail.com>
2023-02-12 12:26:27 +0100

    first commit

---
+++ Hello.js
@@ -0,0 +1,9 @@
+const Hello = () => { 
+    return (
+        <>
+        <h1>Hello world</h1>
+        <p>This is probably my first next app</p>
+        </>
+    )
+} 
+export default Hello;
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoList from './TodoList'
+
+function App() {
+  return (
+   <TodoList/>
+
+  )
+   
+}
+
+export default App;
